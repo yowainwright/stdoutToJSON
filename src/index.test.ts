5bf04a38b4054244cb3e09df27b31df92b1701e1
yowainwright <yowainwright@gmail.com>
2022-03-11 12:51:30 -0800

    feat: adds setup

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,21 @@
+import { stdoutJSON } from './index'
+
+export const STDOUT_MOCK =
+	'{\n' +
+	'  options: { isTestingCLI: true },\n' +
+	"  urls: [ 'https://goodrx.com?gclid=test-clickid' ],\n" +
+	"  cookies: [ { name: 'grx_cm_signal', value: '1' } ],\n" +
+	"  segmentRequest: 'https://segment-api.goodrx.com/v1/'\n" +
+	'}\n'
 
 describe('stringifiedJSON', () => {
-  it('tests', () => {
-    expect(1).toEqual(1);
-  });
-} )
+	it('defaut', () => {
+		const result = stdoutJSON(STDOUT_MOCK)
+		expect(result).toEqual({
+			options: { isTestingCLI: 'true' },
+			urls: ['https://goodrx.com?gclid=test-clickid'],
+			cookies: [{ name: 'grx_cm_signal', value: '1' }],
+			segmentRequest: 'https://segment-api.goodrx.com/v1/',
+		})
+	})
+})
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-
 /**
  * stdoutJson 🕸
  * @description a naive tool useful for outputting stdout as JSON
@@ -9,7 +8,7 @@
 /**
  * @note matches JSON-like shape of unknown values, etc
  */
-export type WithWildcards<T> = T & { [key: string]: unknown }
+export type WithWildcards<T> = T & { [key: string]: unknown };
 /**
  * @note the Matcher shape
  * @param {value} string a string contain a regex pattern to match
@@ -21,23 +20,27 @@ export type Matcher = {
 };
 
 export const OBJECT_MATCHERS: Matcher[] = [
-  { value: 's+', edit: '' }, // remove spaces
-  { value: ':', edit: '":' } // add double quotes to end of a JSON object key
-  { value: '{', edit: '{"' }, // add double quotes to the beginning of JSON object key
-  { value: ',', edit: ',"' }, // add comma to wrap new data item
-]
+  { value: "s+", edit: "" }, // remove +
+  { value: " ", edit: "" }, // remove ' '
+  { value: ":", edit: '":' }, // add double quotes to end of a JSON object key
+  { value: "{", edit: '{"' }, // add double quotes to the beginning of JSON object key
+  { value: ",", edit: ',"' }, // add comma to wrap new data item
+];
 
 export const BOOLEAN_MATCHERS: Matcher[] = [
-  { value: 'true', edit: '"true"' }, // wrap true for JSON parse capability
-	{ value: 'false', edit: '"false"' }, // wrap false for JSON parse capability
+  { value: "true", edit: '"true"' }, // wrap true for JSON parse capability
+  { value: "false", edit: '"false"' }, // wrap false for JSON parse capability
 ];
 
 export const BROWSER_MATCHERS: Matcher[] = [
-  { value: 'https"', edit: 'https' }, // match https after initial pattern match
-  { value: 'http"', edit: 'http' }, // match http after initial pattern match
+  { value: 'https"', edit: "https" }, // match https after initial pattern match
+  { value: 'http"', edit: "http" }, // match http after initial pattern match
 ];
 
-const INITIAL_MATCHERS: Matcher[] = OBJECT_MATCHERS.concat.apply(BOOLEAN_MATCHERS, BROWSER_MATCHERS);
+const INITIAL_MATCHERS: Matcher[] = OBJECT_MATCHERS.concat.apply(
+  BOOLEAN_MATCHERS,
+  BROWSER_MATCHERS
+);
 
 /**
  * matcher
@@ -46,12 +49,15 @@ const INITIAL_MATCHERS: Matcher[] = OBJECT_MATCHERS.concat.apply(BOOLEAN_MATCHER
  * @param {matchers} array
  * @returns {string}
  */
-export function matcher(str: string, matchers: Matcher[] = INITIAL_MATCHERS): string {
-	return matchers.reduce(
-		(updatedStr, { value, edit }) =>
-			updatedStr.replace(new RegExp(value, 'g'), edit),
-		str,
-	)
+export function matcher(
+  str: string,
+  matchers: Matcher[] = INITIAL_MATCHERS
+): string {
+  return matchers.reduce(
+    (updatedStr, { value, edit }) =>
+      updatedStr.replace(new RegExp(value, "g"), edit),
+    str
+  );
 }
 
 /**
@@ -61,21 +67,21 @@ export function matcher(str: string, matchers: Matcher[] = INITIAL_MATCHERS): st
  * @param {matchers} array
  * @returns {object} a JSON object of unknown type
  */
-export function stdoutJson(
-	stdout: string,
-	matchers?: Matcher[],
+export function stdoutJSON(
+  stdout: string,
+  matchers?: Matcher[]
 ): WithWildcards<unknown> {
-	const jsonLikeString = stdout
-		.split('\n') // remove new line chars => []
-		.map((item) => item.trim()) // remove whitespace
-		.filter((item) => item !== '') // filter empty array items
-		.join('') // return the modified string
-
+  const jsonLikeString = stdout
+    .split("\n") // remove new line chars => []
+    .map((item) => item.trim()) // remove whitespace
+    .filter((item) => item !== "") // filter empty array items
+    .join(""); // return the modified string
+  console.log({ jsonLikeString });
   // see the matcher instructions above for detail
-	const stringifiedJSONForParsing = matcher(jsonLikeString, matchers);
-
-	const parsedJSON = JSON.parse(stringifiedJSONForParsing)
-	return parsedJSON
+  const stringifiedJSONForParsing = matcher(jsonLikeString, matchers);
+  console.log({ stringifiedJSONForParsing });
+  const parsedJSON = JSON.parse(stringifiedJSONForParsing);
+  return parsedJSON;
 }
 
-export default stdoutJson
+export default stdoutJSON;
