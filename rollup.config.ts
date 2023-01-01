565b979eb3e4ca4f7368f6720518f9b3feff23d4
yowainwright <yowainwright@gmail.com>
2023-01-01 01:06:09 -0800

    fix: fixes build issue

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,4 +1,4 @@
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import { terser } from "rollup-plugin-terser";
@@ -22,11 +22,11 @@ const banner = `/**
 **/`;
 
 const plugins = [
+  resolve(),
+  commonjs(),
   typescript({
     tsconfig: "tsconfig.json",
   }),
-  nodeResolve(),
-  commonjs(),
   terser(),
 ];
 
